19e2f4acd2aa421fb1907052967368c40081b1a2
Pn\tudo <tu.do@student.passerellesnumeriques.org>
2017-04-05 19:44:13 +0700

    Tu dang tao trang admin dashboard va da nuong bang user

--- cakephp/app/Config/core.php
+++ cakephp/app/Config/core.php
@@ -153,7 +153,7 @@
  *	`admin_index()` and `/admin/controller/index`
  *	`manager_index()` and `/manager/controller/index`
  */
-	//Configure::write('Routing.prefixes', array('admin'));
+Configure::write('Routing.prefixes', array('admin'));
 
 /**
  * Turn off all caching application-wide.
@@ -225,12 +225,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi1');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '768593096574535424967496836451');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
