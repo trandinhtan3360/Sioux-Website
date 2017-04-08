95539c83d6e80ba50464b7f1d08cfe26a6d69eb1
trandinhtan <trandinhtan.web@gmail.com>
2017-04-08 08:39:36 +0700

    update profile user account

--- cakephp/app/Model/User.php
+++ cakephp/app/Model/User.php
@@ -22,16 +22,16 @@ class User extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'full_name' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
+		// 'full_name' => array(
+		// 	'numeric' => array(
+		// 		'rule' => array('numeric'),
+		// 		//'message' => 'Your custom message here',
+		// 		//'allowEmpty' => false,
+		// 		//'required' => false,
+		// 		//'last' => false, // Stop validation after this rule
+		// 		//'on' => 'create', // Limit validation to 'create' or 'update' operations
+		// 	),
+		// ),
 		'username' => array(
 			'notBlank' => array(
 				'rule' => array('notBlank'),
